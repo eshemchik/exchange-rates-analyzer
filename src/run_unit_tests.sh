44458cd236c36d61911a3141d210ca74bb47e049
Evgeny Shemchik <schemtschik@gmail.com>
2024-04-29 17:15:20 +0200

    Update CI.

--- src/run_unit_tests.sh
+++ src/run_unit_tests.sh
@@ -1,5 +1,4 @@
 ./applications/rates_collector_worker_test.py &&
 ./applications/rates_analyzer_worker_test.py &&
 ./applications/backend_test.py &&
-./applications/frontend_test.py &&
-./integration_test.py
+./applications/frontend_test.py
